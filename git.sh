f21680f79ee1fc9c1cb422133cb6b78a570afb74
Paul Tuck <paul.tuck@oxon.org>
2024-01-25 15:01:26 +0700

    "2024-01-25T15:01+07"

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ datetime=$(date "+%Y-%m-%dT%H:%M%Z")
 echo $datetime
 
 # Prompt for a custom commit message
-read -p "Commit meessage: " user_commit_msg
+read -p "Commit message: " user_commit_msg
 
 # Combine the date/time and user's commit message
 full_commit_msg="$datetime - $user_commit_msg"
