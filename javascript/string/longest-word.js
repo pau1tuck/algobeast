6be2a4c356e0c730adfa690031172f58b5bc9dd6
Paul Tuck <paul.tuck@oxon.org>
2024-01-26 19:07:26 +0700

    "2024-01-26T19:07+07"

---
+++ javascript/string/longest-word.js
@@ -0,0 +1,11 @@
+/*** You'll be given a string of words, possibly including punctuation and special characters.
+Your task is to find the longest word within that string.
+
+If there are two or more words of the same length that qualify as the longest, just return the first one you encounter.
+***/
+
+/***
+1. Use a regular expression to strip off non-alphabetic characters and split the string into an array of words.
+2. Iterate through this array, keeping track of the longest word you've encountered so far.
+3. Return the longest word once you've checked them all.
+***/
