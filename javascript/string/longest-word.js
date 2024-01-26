b34fd40716d30d1cd0aa435a2fb0d35d7d46c087
Paul Tuck <paul.tuck@oxon.org>
2024-01-26 21:51:18 +0700

    "2024-01-26T21:51+07"

--- javascript/string/longest-word.js
+++ javascript/string/longest-word.js
@@ -6,6 +6,20 @@ If there are two or more words of the same length that qualify as the longest, j
 
 /***
 1. Use a regular expression to strip off non-alphabetic characters and split the string into an array of words.
-2. Iterate through this array, keeping track of the longest word you've encountered so far.
+2. Iterate through this array using Array.forEach(). Check and keep track of the longest word you've encountered so far in a holding variable (longest)
 3. Return the longest word once you've checked them all.
 ***/
+
+const longestWord = () => {
+    const words = str.replace(/[^a-zA-Z\s]/g, "").split(" ");
+
+    let longest = "";
+    words.forEach(word => {
+        if (word.length > longest.length) {
+            longestWord = word;
+        }
+    });
+
+    return longest;
+};
+console.log(longestWord("Let's find the longest_word in this string!"));
