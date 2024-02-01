ad3b8eecede9eaab3e308a1cee56fe7b000ed17e
Paul Tuck <paul.tuck@oxon.org>
2024-02-01 17:10:09 +0700

    "2024-02-01T17:09+07"

---
+++ javascript/string/find-duplicates.js
@@ -0,0 +1,22 @@
+//! FIND DUPLICATES ***/
+/* You're given a string that represents a list of names, separated by commas.
+/* Write a function that finds and returns the FIRST name that appears more than once in the list. If no name is repeated, return a message. */
+
+findDuplicate = names => {
+    const nameCounts = {};
+    const nameList = names.split(",");
+
+    for (const name of nameList) {
+        nameCounts[name] = (nameCounts[name] || 0) + 1;
+        if (nameCounts[name] > 1) {
+            return name; // Duplicate found
+        }
+    }
+
+    return "No duplicates found."; // No duplicates found
+};
+/*
+Time Complexity: O(n) - This is because you're looping through the list of names once. Each operation within the loop (checking and updating the hashmap) is pretty much instantaneous (O(1)), thanks to the magic of hashmaps allowing rapid-fire access to stored data. So, the time it takes is directly proportional to the number of names in your list.
+
+Space Complexity: O(n) - In the worst-case scenario, if all names are unique, your hashmap will end up storing each name once. So, the space needed grows linearly with the number of names, hence O(n).
+*/
--- javascript/string/longest-word.js
+++ javascript/string/longest-word.js
@@ -1,3 +1,4 @@
+//! LONGEST WORD ***/
 /*** You'll be given a string of words, possibly including punctuation and special characters.
 Your task is to find the longest word within that string.
 
@@ -7,7 +8,7 @@ If there are two or more words of the same length that qualify as the longest, j
 /***
 1. Use a regular expression to strip off non-alphabetic characters and split the string into an array of words.
 2. Iterate through this array using Array.forEach(). Check and keep track of the longest word you've encountered so far in a holding variable (longest)
-3. Return the longest word once you've checked them all.
+3. Return the longest word once you've checked each.
 ***/
 
 const longestWord = () => {
