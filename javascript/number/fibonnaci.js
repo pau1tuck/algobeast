f1b4972b3c1dcbc57bf6caaf25b18d839eacba24
Paul Tuck <paul.tuck@oxon.org>
2024-02-07 20:02:28 +0700

    $user_commit_msg"

--- javascript/number/fibonnaci.js
+++ javascript/number/fibonnaci.js
@@ -1,6 +1,5 @@
-const fibonacci = num => { // recursion
-    if (num <= 1) return num;
-    else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
-    }
+const fibonacci = (num) => {
+	// recursion
+	if (num <= 1) return num;
+	return fibonacci(num - 1) + fibonacci(num - 2);
 }; // O(2^n)
