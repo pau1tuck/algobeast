c4d6329c640485536e27317ddd45fd63754fb04f
Paul Tuck <paul.tuck@oxon.org>
2024-03-24 16:39:37 +0700

    $user_commit_msg"

--- javascript/arrays/sort/insertion-sort.js
+++ javascript/arrays/sort/insertion-sort.js
@@ -1,14 +1,15 @@
 function selectionSort(array) {
     for (let i = 0; i < array.length; i++) {
-        // Set minimum to this position
         let min = i;
-        // Check the rest of the array to see if anything is smaller
+
+        // Check the rest of the array to see if anything is smaller:
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[min]) {
                 min = j;
             }
         }
-        // If the minimum isn't in the position, swap it
+
+        // If the minimum isn't in the position, swap it:
         if (i !== min) {
             const swap = array[i];
             array[i] = array[min];
