bc0567748d7c777c1e1142d29d4d5959e8c97546
Paul Tuck <paul.tuck@oxon.org>
2024-03-25 15:25:05 +0700

    add n-th fibonnaci recursive

--- javascript/number/fibonnaci.js
+++ javascript/number/fibonnaci.js
@@ -6,12 +6,17 @@ const fibonacciRecursive = num => {
 /* Problem: Calculate the n-th Fibonacci number using an iterative approach. */
 
 const nthFibonacciIterative = (num) => {
-	let a = 0;
-	let b = 1;
-	for (let i = 0; i < num; i++) {
-		const tmp = a;
-		a = b;
-		b = tmp + b;
-	}
-	return b;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < num; i++) { // Starting from 1 since we already defined the first two numbers
+        const tmp = a;
+        a = b;
+        b = tmp + b;
+    }
+    return a; // Return 'a' for the n-th number
 }; // O(n) time, O(1) space/memory
+
+const nthFibonacciRecursive = (num) => {
+	if (num <= 1) return num; // Base cases: return 'num' if it's 0 or 1
+	return nthFibonacciRecursive(num - 1) + nthFibonacciRecursive(num - 2);
+  };
