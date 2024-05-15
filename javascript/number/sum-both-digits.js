0ae5ac1aae82aedade243c3599d254e433372c36
Paul Tuck <paul.tuck@oxon.org>
2024-05-15 18:11:33 +0700

    feat: number of candies

--- javascript/number/sum-both-digits.js
+++ javascript/number/sum-both-digits.js
@@ -1,9 +1,8 @@
 /* Given a two-digit integer n, add both digits together.
 For example, n = 23 returns 5. */
 
-//? O(1) time | O(1) space
-
 function addTwoDigits(n) {
     return Math.floor(n / 10) + n % 10;
 }
+//? O(1) time | O(1) space
 
