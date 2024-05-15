4a97cf9dbb8db39f6cdc66485b955397be9ba08b
Paul Tuck <paul.tuck@oxon.org>
2024-05-16 00:41:15 +0700

    feat: number of candies solution

--- javascript/number/candies.js
+++ javascript/number/candies.js
@@ -2,12 +2,12 @@
 They want to eat as much candy as they can, but each child must eat exactly the same amount of candy as any other child.
 Determine how many pieces of candy will be eaten by all of the children together. Individual pieces of candy cannot be split. */
 
-function shareCandies2(n, m) {
-    return m - (m % n);
+function shareCandies(n, m) { // e.g. 3, 7
+    return m - (m % n); // e.g. 7 - (7 % 3 = 1) = 6
 }
 //  O(1) time | O(1) space
 
-function shareCandies1(n, m) {
+function shareCandies2(n, m) {
     let children = n;
     let candy = m;
     return Math.floor(candy / children) * children;
