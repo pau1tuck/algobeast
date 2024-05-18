7ccf3a5c93245b6eef8760c8b06b1d66ab1b8ae7
Paul Tuck <paul.tuck@oxon.org>
2024-05-19 01:37:36 +0700

    feat: undo-redo stack

--- javascript/stacks/undo-redo.js
+++ javascript/stacks/undo-redo.js
@@ -1,33 +1,30 @@
-class UndoStack {
+class undoneStack {
     constructor() {
         this.currentStack = [];
-        this.undoStack = [];
+        this.undoneStack = [];
     }
     // Perform an action and save state
     performAction(state) {
         this.currentStack.push(state);
         // Clear the undo stack because we can no longer redo after a new action
-        this.undoStack = [];
+        this.undoneStack = [];
     }
-    // Undo the last action
     undo() {
         if (this.currentStack.length > 0) {
             const state = this.currentStack.pop();
-            this.undoStack.push(state);
+            this.undoneStack.push(state);
             return state;
         }
         return null; // No state to undo
     }
-    // Redo the last undone action
     redo() {
-        if (this.undoStack.length > 0) {
-            const state = this.undoStack.pop();
+        if (this.undoneStack.length > 0) {
+            const state = this.undoneStack.pop();
             this.currentStack.push(state);
             return state;
         }
         return null; // No state to redo
     }
-    // Get the current state
     getCurrentState() {
         if (this.currentStack.length > 0) {
             return this.currentStack[this.currentStack.length - 1];
@@ -35,12 +32,11 @@ class UndoStack {
         return null; // No current state
     }
 }
-
-const undoStack = new UndoStack();
-undoStack.performAction("State1");
-undoStack.performAction("State2");
-console.log(undoStack.getCurrentState()); // Output: "State2"
-console.log(undoStack.undo()); // Output: "State2"
-console.log(undoStack.getCurrentState()); // Output: "State1"
-console.log(undoStack.redo()); // Output: "State2"
-console.log(undoStack.getCurrentState()); // Output: "State2"
+const undoneStack = new undoneStack();
+undoneStack.performAction("State1");
+undoneStack.performAction("State2");
+console.log(undoneStack.getCurrentState()); // Output: "State2"
+console.log(undoneStack.undo()); // Output: "State2"
+console.log(undoneStack.getCurrentState()); // Output: "State1"
+console.log(undoneStack.redo()); // Output: "State2"
+console.log(undoneStack.getCurrentState()); // Output: "State2"
