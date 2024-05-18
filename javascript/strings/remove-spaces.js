4dab7e1f7227c88d9d1f521496231956e2988393
Paul Tuck <paul.tuck@oxon.org>
2024-05-18 22:12:16 +0700

    feat: remove spaces

--- javascript/strings/remove-spaces.js
+++ javascript/strings/remove-spaces.js
@@ -1,21 +1,21 @@
 function removeSpaces1(str) {
     return str.split(" ").join("");
-}
+} // Time: O(n), Space: O(n)
 
 function removeSpaces2(str) {
     return str.replace(/\s+/g, "");
-}
+} // Time: O(n), Space: O(n)
 
 function removeSpaces3(str) {
     return str.replaceAll(" ", "");
-}
+} // Time: O(n), Space: O(n)
 
 function removeSpaces4(str) {
     return str
         .split("")
         .filter(char => char !== " ")
         .join("");
-}
+} // Time: O(n), Space: O(n)
 
 function removeSpaces5(str) {
     return str.split("").reduce((acc, char) => {
@@ -24,4 +24,5 @@ function removeSpaces5(str) {
         }
         return acc;
     }, "");
-}
+} // Time: O(n), Space: O(n)
+s;
