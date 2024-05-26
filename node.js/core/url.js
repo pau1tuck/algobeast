6b216400b53de1f89e4d39015578c65d2aac92b9
Paul Tuck <paul.tuck@oxon.org>
2024-05-26 18:05:27 +0700

    feat: add url reference sheet

--- node.js/core/url.js
+++ node.js/core/url.js
@@ -1,13 +1,8 @@
 import { URL, URLSearchParams } from 'node:url';
-
-/*
-The `url` module provides utilities for URL resolution and parsing. It allows for the
-construction, parsing, and encoding/decoding of URLs. The module offers both a legacy API
-and a modern, standardized API based on the WHATWG URL specification.
-*/
+/* The `url` module provides utilities for URL resolution and parsing. It allows for the construction, parsing, and encoding/decoding of URLs. The module offers both a legacy API and a modern, standardized API based on the WHATWG URL specification. */
 
 // Constructing a new URL
-const myURL = new URL('https://example.com:8080/pathname?query=value#hash');
+const myUrl = new URL('https://example.com:8080/pathname?query=value#hash');
 console.log('URL:', myURL.href);
 
 // Access different parts of the URL
@@ -26,6 +21,8 @@ console.log('Hash:', myURL.hash);  // '#hash'
 const params = new URLSearchParams(myURL.search);
 console.log('Has query:', params.has('query'));  // true
 console.log('Value of "query":', params.get('query'));  // 'value'
+myUrl.searchParams.set('search', 'newvalue');
+console.log(myUrl.href); // '<https://example.com/newpath/?search=newvalue#hash>'
 params.append('newKey', 'newValue');
 console.log('Updated Search:', params.toString());  // 'query=value&newKey=newValue'
 
@@ -40,3 +37,18 @@ console.log('Legacy URL:', legacyURL.href);
 
 // Note: It's recommended to use the WHATWG URL API (new URL and URLSearchParams) instead
 // of the legacy API for new projects.
+
+const urlObject = {
+    href: 'https://www.example.com:8080/path/name?query=search#hash',
+    origin: 'https://www.example.com:8080',
+    protocol: 'https:',
+    username: '',
+    password: '',
+    host: 'www.example.com:8080',
+    hostname: 'www.example.com',
+    port: '8080',
+    pathname: '/path/name',
+    search: '?query=search',
+    searchParams: { 'query': 'search' },
+    hash: '#hash'
+}
