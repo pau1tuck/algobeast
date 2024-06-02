c1a3907b81ded9134b97554ccdc164835d2e0a13
Paul Tuck <paul.tuck@oxon.org>
2024-06-02 15:51:18 +0700

    feat: improve reverse only letters solution

--- javascript/strings/reverse-only-letters.js
+++ javascript/strings/reverse-only-letters.js
@@ -1,4 +1,21 @@
 function reverseOnlyLetters(S) {
+    let arr = S.split('');
+    let left = 0, right = arr.length - 1;
+    while (left < right) {
+        if (!/[a-zA-Z]/.test(arr[left])) {
+            left++;
+        } else if (!/[a-zA-Z]/.test(arr[right])) {
+            right--;
+        } else {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    return arr.join('');
+} // Time: O(n), Space: O(1)
+
+function reverseOnlyLetters(S) {
     let letters = S.split('').filter(c => /[a-zA-Z]/.test(c));
     let result = [];
     for (let i = 0; i < S.length; i++) {
